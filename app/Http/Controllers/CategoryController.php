4820afcf02b4aebd6a558537e1a44b02dccb7ef9
harisanwar129 <harisanwar129@gmail.com>
2021-08-13 06:39:32 +0700

    22.Integrasi Categories Page

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Kategori;
+use App\Produk;
 class CategoryController extends Controller
 {
   /**
@@ -13,6 +14,29 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        return view('pages.category');
+
+        $categories=Kategori::all();
+        $products=Produk::with(['galleri'])->paginate(32);
+        return view('pages.category',[
+            'categories'=>$categories,
+            'products'=>$products
+        ]);
+    }
+/**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+      public function detail(Request $request,$slug)
+    {
+
+        $categories=Kategori::all();
+        $category=Kategori::where('slug',$slug)->firstOrFail();
+        $products=Produk::with(['galleri'])->where('kategori_id',$category->id)->paginate(32);
+    
+        return view('pages.category',[
+            'categories'=>$categories,
+            'products'=>$products
+        ]);
     }
 }
